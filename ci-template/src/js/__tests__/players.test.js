d94fb8258de1b8d3b06716589679c5a84b61c526
Dilara Gilfanova <dgilfanova12@gmail.com>
2025-12-11 02:59:02 +0300

    second commit

--- ci-template/src/js/__tests__/players.test.js
+++ ci-template/src/js/__tests__/players.test.js
@@ -1,11 +1,5 @@
 import playerHealth from '../players';
 
-// test('should check health', () => {
-//     let result = 'healthy';
-//     expect(result).toBe('healthy');
-// });
-
-
 test('should healthy for health > 50', () => {
     expect(playerHealth({name: 'Маг', health: 90})).toBe('healthy');
 });
@@ -17,14 +11,4 @@ test('should wounded for health >= 15 and <= 50', () => {
 test('should wounded for health < 15', () => {
     expect(playerHealth({name: 'Ron', health: 10})).toBe('critical');
 });
-
-// export function playerHealth (player) {
-//     if (player.health > 50) {
-//         return 'healthy';
-//     } else if (player.health >= 15 && player.health <= 50){
-//         return 'wounded';
-//     } else {
-//         return 'critical'
-//     }
-// }
 
